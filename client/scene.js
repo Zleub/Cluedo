bde180892ec36502ea53147bce54cbfa8fc78bb8
Zleub <debray.arnaud@gmail.com>
2017-06-06 18:58:49 +0200

    client work

--- client/scene.js
+++ client/scene.js
@@ -1,3 +1,17 @@
+//           `--::-.`
+//       ./shddddddddhs+.
+//     :yddddddddddddddddy:
+//   `sdddddddddddddddddddds`
+//  /ddddy:oddddddddds:sddddd/   @By: Debray Arnaud <adebray> - adebray@student.42.fr
+//  sdddddddddddddddddddddddds   @Last modified by: adebray
+//  sdddddddddddddddddddddddds
+//  :ddddddddddhyyddddddddddd:   @Created: 2017-06-06T01:07:54+02:00
+//   odddddddd/`:-`sdddddddds    @Modified: 2017-06-06T01:16:44+02:00
+//    +ddddddh`+dh +dddddddo
+//     -sdddddh///sdddddds-
+//       .+ydddddddddhs/.
+//           .-::::-`
+
 canvas = document.querySelector('canvas')
 scene = new THREE.Scene()
 camera = new THREE.PerspectiveCamera( 75, canvas.width / canvas.height, 0.1, 1000 )
