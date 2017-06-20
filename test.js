d32e1801a83efb81ccd10304428f48b38d48e130
Zleub <debray.arnaud@gmail.com>
2017-06-20 21:44:44 +0200

    test

---
+++ test.js
@@ -0,0 +1,15 @@
+//           `--::-.`
+//       ./shddddddddhs+.
+//     :yddddddddddddddddy:
+//   `sdddddddddddddddddddds`
+//  /ddddy:oddddddddds:sddddd/   @By: Debray Arnaud <adebray> - adebray@student.42.fr
+//  sdddddddddddddddddddddddds   @Last modified by: adebray
+//  sdddddddddddddddddddddddds
+//  :ddddddddddhyyddddddddddd:   @Created: 2017-06-20T21:44:16+02:00
+//   odddddddd/`:-`sdddddddds    @Modified: 2017-06-20T21:44:25+02:00
+//    +ddddddh`+dh +dddddddo
+//     -sdddddh///sdddddds-
+//       .+ydddddddddhs/.
+//           .-::::-`
+
+console.log('Starting test.')
