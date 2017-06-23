8aa77d4d57d95f02cbb35f5da440cb5c67321ff3
Zleub <debray.arnaud@gmail.com>
2017-06-23 16:25:46 +0200

    more tests routes

---
+++ server/routes/generate.js
@@ -0,0 +1,17 @@
+//           `--::-.`
+//       ./shddddddddhs+.
+//     :yddddddddddddddddy:
+//   `sdddddddddddddddddddds`
+//  /ddddy:oddddddddds:sddddd/   @By: Debray Arnaud <adebray> - adebray@student.42.fr
+//  sdddddddddddddddddddddddds   @Last modified by: adebray
+//  sdddddddddddddddddddddddds
+//  :ddddddddddhyyddddddddddd:   @Created: 2017-06-22T21:28:21+02:00
+//   odddddddd/`:-`sdddddddds    @Modified: 2017-06-23T13:50:47+02:00
+//    +ddddddh`+dh +dddddddo
+//     -sdddddh///sdddddds-
+//       .+ydddddddddhs/.
+//           .-::::-`
+
+exports.get = function () {
+	this.res.end("Hello generate get !\n")
+}
--- server/routes/story.js
+++ server/routes/story.js
@@ -6,18 +6,32 @@
 //  sdddddddddddddddddddddddds   @Last modified by: adebray
 //  sdddddddddddddddddddddddds
 //  :ddddddddddhyyddddddddddd:   @Created: 2017-06-20T20:59:09+02:00
-//   odddddddd/`:-`sdddddddds    @Modified: 2017-06-20T22:17:14+02:00
+//   odddddddd/`:-`sdddddddds    @Modified: 2017-06-23T16:17:36+02:00
 //    +ddddddh`+dh +dddddddo
 //     -sdddddh///sdddddds-
 //       .+ydddddddddhs/.
 //           .-::::-`
 
 exports.get = function () {
-	this.res.end("Hello World !\n")
+	this.res.end("Hello story get !\n")
+}
+
+exports.post = function () {
+	this.res.end("Hello story post !\n")
 }
 
 exports['/generate'] = {
-	on: function () {
-		this.res.end("Hello World !\n")
+	get: function () {
+		this.res.end("Hello story/generate get !\n")
+	}
+}
+
+exports['/:param'] = {
+	get: function (param) {
+		this.res.end(`Hello story/param <${param}> get !\n`)
+	},
+
+	"/:more_param": function (more_param) {
+		this.res.end(`Hello story/more_param <${more_param}> get !\n`)
 	}
 }
